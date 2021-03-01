06803f407e0f92b335657aaf1d3d661aab3fb63e
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2021-03-01 15:29:39 +0900

    검색창 영역 클론 코딩

--- dev-matching/site/src/App.js
+++ dev-matching/site/src/App.js
@@ -1,5 +1,39 @@
+import SearchingSection from './components/SearchingSection.js';
+
+import api from './api/catApi.js';
+import { getItem, setItem } from './util/sessionStorage.js';
+
 export default class App {
     constructor($target) {
-        console.log($target);
+        const keywords = getItem('keywords');
+        const data = getItem('data');
+
+        const searchingSection = new SearchingSection({
+            $target,
+            keywords,
+            onSearch: async keyword => {
+                const response = await api.fetchCats(keyword);
+
+                if (response.isError) {
+                    setItem('data', response.data);
+                } else {
+                    console.log(response.data);
+                    // error.setState(response.data);
+                }
+            },
+            onRandom: async () => {
+                const response = await api.fetchRandomCats();
+                if(!response.isError){
+                    setItem('data', response.data);
+                } else {
+                    // error.setState(response.data);
+                }
+            }
+        });
+
+        const error = new Error({
+            $target
+        });
+
     }
 }
