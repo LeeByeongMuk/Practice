7f98c766105cf7cdc76ba8584bad908fa75d7c01
LeeByeongMuk <mansogod@gmail.com>
2021-04-16 17:05:03 +0900

    숫자 야구 만들기 작업

---
+++ react/숫자야구/client.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import {hot} from 'react-hot-loader/root';
+
+import NumberBaseBall from './NumberBaseBall';
+
+const Hot = hot(NumberBaseBall);
+
+ReactDom.render(<Hot />, document.querySelector('#root'));
