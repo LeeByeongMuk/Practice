ec049d964795f489b4947e6267dc787693bba80e
LeeByeongMuk <mansogod@gmail.com>
2021-04-17 23:06:08 +0900

    지뢰찾기 만들기 작업

---
+++ react/지뢰찾기/Table.jsx
@@ -0,0 +1,20 @@
+import React, {memo, useContext} from "react";
+
+import Tr from "./Tr";
+import {TableContext} from "./MineSearch";
+
+const Table = memo(() => {
+    const {tableData} = useContext(TableContext);
+    return (
+        <table>
+            <tbody>
+                {Array(tableData.length).fill('').map((tr, i) => {
+                    return <Tr key={tableData[i] + i}
+                               rowIndex={i}/>;
+                })}
+            </tbody>
+        </table>
+    );
+});
+
+export default Table;
---
+++ react/지뢰찾기/Td.jsx
@@ -0,0 +1,127 @@
+import React, {memo, useContext, useMemo, useCallback} from "react";
+import {CODE, OPEN_CELL, CLICK_MINE, FLAG_CELL, QUESTION_CELL, NORMALIZE_CELL, TableContext} from "./MineSearch";
+
+const getTdStyle = (code) => {
+    switch (code) {
+        case CODE.NORMAL:
+        case CODE.MINE:
+            return {
+                background: '#444'
+            };
+        case CODE.OPENED:
+            return {
+                background: '#fff'
+            };
+        case CODE.QUESTION:
+        case CODE.QUESTION_MINE:
+            return {
+                background: 'yellow'
+            };
+        case CODE.FLAG:
+        case CODE.FLAG_MINE:
+            return {
+                background: 'red'
+            };
+        default:
+            return {
+                background: '#fff'
+            };
+    }
+};
+
+const getTdText = (code) => {
+    switch (code) {
+        case CODE.NORMAL:
+            return '';
+        case CODE.MINE:
+            return 'x';
+        case CODE.CLICKED_MINE:
+            return '펑';
+        case CODE.FLAG:
+        case CODE.FLAG_MINE:
+            return '!';
+        case CODE.QUESTION:
+        case CODE.QUESTION_MINE:
+            return '?';
+        default:
+            return code || '';
+    }
+};
+
+const Td = memo(({rowIndex, cellIndex}) => {
+    const {tableData, halted, dispatch} = useContext(TableContext);
+
+    const onClickTd = useCallback(() => {
+        if (halted) return;
+
+        switch (tableData[rowIndex][cellIndex]) {
+            case CODE.OPENED:
+            case CODE.FLAG_MINE:
+            case CODE.FLAG:
+            case CODE.QUESTION_MINE:
+            case CODE.QUESTION:
+                return;
+            case CODE.NORMAL:
+                dispatch({
+                    type: OPEN_CELL,
+                    row: rowIndex,
+                    cell: cellIndex
+                });
+                return;
+            case CODE.MINE:
+                dispatch({
+                    type: CLICK_MINE,
+                    row: rowIndex,
+                    cell: cellIndex
+                });
+                return;
+        }
+
+    }, [tableData[rowIndex][cellIndex], halted]);
+
+    const onRightClickTd = useCallback((e) => {
+        e.preventDefault();
+
+        if (halted) return;
+
+        switch (tableData[rowIndex][cellIndex]) {
+            case CODE.NORMAL:
+            case CODE.MINE:
+                dispatch({
+                    type: FLAG_CELL,
+                    row: rowIndex,
+                    cell: cellIndex
+                });
+                return;
+            case CODE.FLAG:
+            case CODE.FLAG_MINE:
+                dispatch({
+                    type: QUESTION_CELL,
+                    row: rowIndex,
+                    cell: cellIndex
+                });
+                return;
+            case CODE.QUESTION:
+            case CODE.QUESTION_MINE:
+                dispatch({
+                    type: NORMALIZE_CELL,
+                    row: rowIndex,
+                    cell: cellIndex
+                });
+                return;
+            default:
+                return;
+        }
+
+    }, [tableData[rowIndex][cellIndex], halted]);
+
+    return useMemo(() => (
+        <td style={getTdStyle(tableData[rowIndex][cellIndex])}
+            onClick={onClickTd}
+            onContextMenu={onRightClickTd}>
+            {getTdText(tableData[rowIndex][cellIndex])}
+        </td>
+    ), [tableData[rowIndex][cellIndex]]);
+});
+
+export default Td;
