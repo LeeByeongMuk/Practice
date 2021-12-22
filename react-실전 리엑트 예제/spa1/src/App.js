59fe7d95d728edc64788814345c1f33e4a871724
LeeByeongMuk <mansogod@gmail.com>
2021-12-23 00:06:53 +0900

    실전 리액트 spa 예제

---
+++ react-실전 리엑트 예제/spa1/src/App.js
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from 'react';
+
+function App() {
+    const [pageName, setPageName] = useState('');
+
+    useEffect(() => {
+        window.onpopstate = (event) => {
+            console.log(document.location, event.state);
+        };
+    }, []);
+
+    function onReloadPage(data, pageName) {
+        window.history.pushState(data, '', `/${pageName}`);
+        setPageName(pageName);
+    }
+
+    return (
+        <div>
+            <button onClick={() => onReloadPage('v1', 'page1')}>
+                page1
+            </button>
+            <button onClick={() => onReloadPage('v2', 'page2')}>
+                page1
+            </button>
+
+            {!pageName && <Home/>}
+            {pageName === 'page1' && <Page1/>}
+            {pageName === 'page2' && <Page2/>}
+        </div>
+    );
+}
+
+function Home() {
+    return <h1>'홈'</h1>;
+}
+
+function Page1() {
+    return <h1>'page1'</h1>;
+}
+
+function Page2() {
+    return <h1>'page2'</h1>;
+}
+
+export default App;
