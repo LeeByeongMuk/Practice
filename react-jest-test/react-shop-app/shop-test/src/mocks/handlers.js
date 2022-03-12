7abf173a6fedd2444172f3259f35baa4bc81a87d
LeeByeongMuk <mansogod@gmail.com>
2022-03-12 23:29:06 +0900

    mock service worker 설정

---
+++ react-jest-test/react-shop-app/shop-test/src/mocks/handlers.js
@@ -0,0 +1,33 @@
+import {rest} from "msw";
+
+const URL = 'http://localhost:5000';
+
+export const handlers = [
+  rest.get(`${URL}/products`, (req, res, ctx) => {
+    return res (
+      ctx.json([
+        {
+          name: 'America',
+          imagePath: '/images/america.jpg',
+        },
+        {
+          name: 'England',
+          imagePath: '/images/england.jpg',
+        }
+      ])
+    )
+  }),
+  rest.get(`${URL}/options`, (req, res, ctx) => {
+    return res (
+      ctx.json([
+        {
+          name: 'Insurance',
+        },
+        {
+          name: 'Dinner'
+        }
+      ])
+    )
+  }),
+
+]
