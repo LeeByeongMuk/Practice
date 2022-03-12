127941c1975a7e8847287064af0e6211836d837d
LeeByeongMuk <mansogod@gmail.com>
2022-03-12 21:48:25 +0900

    counter app test code

--- react-jest-test/counter-app/src/App.js
+++ react-jest-test/counter-app/src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h3 data-testid="counter">{counter}</h3>
+      <div>
+        <button
+          data-testid="minus-button"
+          disabled={isDisabled}
+          onClick={() => {
+            setCounter(counter - 1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          -
+        </button>
+        <button
+          data-testid="plus-button"
+          disabled={isDisabled}
+          onClick={() => {
+            setCounter(counter + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
+      <div>
+        <button
+          data-testid="on/off-button"
+          style={{
+            backgroundColor: 'blue'
+          }}
+          onClick={() => setIsDisabled(prev => !prev)}
+        >
+          on/off
+        </button>
+      </div>
     </div>
   );
 }
